58217407e38c6853f791d953b4cdb6f8a451dae3
Greg Johnson <greg.johnson@saltaire.com.au>
2021-09-21 10:22:11 +1000

    fix function name

--- src/utils/yuml2dot-utils.js
+++ src/utils/yuml2dot-utils.js
@@ -138,7 +138,7 @@ module.exports = function()
         if (es != null && es.length == 3)
         {
             estype = es[2].trim().toLowerCase()
-            c = event_storming_color(es[2])
+            c = map_event_storming_color(es[2])
             return [es[1] + " {bg: " + c + "}", estype];
         } 
         else
